3b4b5af8071ec90ac8de0990ac11780195c57d8f
NadiaOl <nadia.olifirova@gmail.com>
2023-03-11 20:38:23 +0200

    ДЗ на проверку

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,28 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+//получаем доступ
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
     
+// получаем данные для хранилища
 const getPosition = function (currentPosition) {
 localStorage.setItem("videoplayer-current-time", JSON.stringify(currentPosition));
 }
 
 const playerON = player.on('timeupdate', throttle(getPosition, 1000))
 
-
-
+//автовоспроизведение
 const pausedTime = localStorage.getItem("videoplayer-current-time");
 const startTime = JSON.parse(pausedTime);
 
 player.setCurrentTime(startTime.seconds||0).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
+
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
             break;
-
-        default:
-            // some other error occurred
+            default:
             break;
     }
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,30 @@
 import throttle from 'lodash.throttle';
 
+// переменная для ключа хранилища
 const STORAGE_KEY = 'feedback-form-state';
 
+// получаем доступ
 const refs = {
     form: document.querySelector('.feedback-form'),
     input: document.querySelector('.feedback-form  input'),
     textarea: document.querySelector('.feedback-form  textarea'),
 };
+
+// создаем объкт {email: ...., message: .... }
 const formData = {};
 
+//добавляем слушателей
 refs.form.addEventListener('submit', handelFormSubmit);
 refs.form.addEventListener('input', throttle(handelInput, 500));
 
+// получаем данные для хранилища
 function handelInput(event) {
     formData[event.target.name] = event.target.value;
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
+//обнуляемся при сабмите
 populateTextarea();
 
 function handelFormSubmit(event) {
@@ -28,6 +35,7 @@ function handelFormSubmit(event) {
     console.log(formData);
 }
 
+//заполняем поля "автосохранение"
 function populateTextarea() {
     const savedMessage = localStorage.getItem(STORAGE_KEY);
     const savedMessageParse = JSON.parse(savedMessage);
